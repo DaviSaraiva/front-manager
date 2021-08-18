436e87257aa65cc6125ee5cad55a7bff2faffeee
DaviSaraiva <davi123467890@gmail.com>
2021-08-17 23:42:18 -0300

    finalizacao o componente de input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,67 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {
+  ComponentType,
+  InputHTMLAttributes,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { IconBaseProps } from 'react-icons';
+import { Container } from './styles';
 
-// import { Container } from './styles';
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon: ComponentType<IconBaseProps>;
+  isPassword?: boolean;
+}
 
-const Input: React.FC = () => {
-  return <div />;
+const Input: React.FC<IProps> = ({
+  icon: Icon,
+  isPassword = false,
+  ...rest
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={16} />}
+      <input
+        ref={inputRef}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+        type={showPassword ? 'text' : rest.type}
+      />
+      {isPassword &&
+        (showPassword ? (
+          <FaEyeSlash
+            size={16}
+            color="#00e676"
+            onClick={() => setShowPassword(!showPassword)}
+            className="icon-click"
+          />
+        ) : (
+          <FaEye
+            size={16}
+            color="#00e676"
+            onClick={() => setShowPassword(!showPassword)}
+            className="icon-click"
+          />
+        ))}
+    </Container>
+  );
 };
 
 export default Input;
--- src/pages/SingInDev/index.tsx
+++ src/pages/SingInDev/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaEnvelope, FaKey } from 'react-icons/fa';
+import Input from '../../components/Input';
 import { Container, Content, Background, Form } from './styles';
 
 const SingInDev: React.FC = () => {
@@ -8,6 +10,13 @@ const SingInDev: React.FC = () => {
         <Content>
           <Form>
             <h1>Login com Dev</h1>
+            <Input icon={FaEnvelope} placeholder="Email" type="email" />
+            <Input
+              icon={FaKey}
+              placeholder="Senha"
+              isPassword
+              type="password"
+            />
           </Form>
         </Content>
         <Background />
